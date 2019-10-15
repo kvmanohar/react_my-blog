41f8abd114322f911208d09581d20ffeba9b1d0b
Manohar Kurapati <mankurap@wkmgb0012442.global.publicisgroupe.net>
2019-10-15 16:08:13 +0100

    Article lists page with links completed

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => (
         <Link to="/about">About</Link>
       </li>
       <li>
-        <Link to="/article-list">Articles</Link>
+        <Link to="/articles-list">Articles</Link>
       </li>
     </ul>
   </nav>
