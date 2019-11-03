632249a2df904cbd72d8e46eb9757b7aca2a500a
Manohar Kurapati <kvmanohar@gmail.com>
2019-11-03 10:45:36 +0000

    Upvotes & commentsLIst added successfully

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,25 +1,42 @@
-import React from "react";
-import articleContent from "./article-content";
-import ArticlesList from "../components/ArticlesList";
-import PageNotFound from "./NotFoundPage";
+import React, { useState, useEffect } from 'react';
+import articleContent from './article-content';
+import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvoteSection from '../components/UpvoteSection';
+import PageNotFound from './NotFoundPage';
+
 
 const ArticlePage = ({ match }) => {
-  const name = match.params.name;
-  const article = articleContent.find(article => article.name === name);
+	const name = match.params.name;
+	const article = articleContent.find((article) => article.name === name);
 
-  if (!article) return <PageNotFound />;
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-  const otherArticles = articleContent.filter(article => article.name !== name);
-  return (
-    <>
-      <h1>{article.title}</h1>
-      {article.content.map((paragraph, key) => (
-        <p key={key}> {paragraph} </p>
-      ))}
-      <h3>Other Articles: </h3>
-      <ArticlesList articles={otherArticles} />
-    </>
-  );
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`/api/articles/${name}`)
+			const body = await result.json();
+			setArticleInfo(body);
+		}
+		fetchData();
+
+	},[name]);
+
+	if (!article) return <PageNotFound />;
+
+	const otherArticles = articleContent.filter((article) => article.name !== name);
+	return (
+		<>
+			<h1>{article.title}</h1>
+			<UpvoteSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
+			{article.content.map((paragraph, key) => (
+				<p key={key}> {paragraph} </p>
+			))}
+			<CommentsList comments={articleInfo.comments} />
+			<h3>Other Articles: </h3>
+			<ArticlesList articles={otherArticles} />
+		</>
+	);
 };
 
 export default ArticlePage;
